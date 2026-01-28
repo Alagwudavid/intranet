0ff7dc6d30d7863d3252d9c67b46ce4f4cb30d16
David (Favour) Alagwu <57830927+Alagwudavid@users.noreply.github.com>
2026-01-28 05:18:03 +0100

    fix: correct import paths for Navbar, Sidebar, and SearchBar components

--- app/me/courses/layout.tsx
+++ app/me/courses/layout.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
-import Navbar from "/components/navbar";
-import Sidebar from "/components/sidebar";
+import Navbar from "../components/navbar";
+import Sidebar from "../components/sidebar";
 
 export const metadata: Metadata = {
   title: "Courses | Bitroot intranet",
