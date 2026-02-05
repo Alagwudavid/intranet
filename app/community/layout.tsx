8ed2fd74576075dde8fc46f4b31271df2482dbe9
David (Favour) Alagwu <57830927+Alagwudavid@users.noreply.github.com>
2026-02-05 06:20:57 +0100

    update ui

--- app/community/layout.tsx
+++ app/community/layout.tsx
@@ -2,6 +2,8 @@ import { Metadata } from "next";
 import Navbar from "@/app/components/navbar";
 import Sidebar from "@/app/components/sidebar";
 import Footer from "../components/footer";
+import AppContent from "../components/app-content";
+import FooterMini from "../components/footer-mini";
 
 export const metadata: Metadata = {
   title: "Community | Intranet web",
@@ -14,13 +16,15 @@ export default function CommunityLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Navbar />
+    <div className="w-full h-screen flex items-center gap-0 bg-muted">
       <Sidebar />
-      <div className="min-h-page bg-muted lg:pl-20 pb-12 lg:pb-0">
-        {children}
-        <Footer />
+      <div className="flex-1 flex flex-col w-[calc(100%-88px)] h-screen relative bg-muted pr-2">
+        <AppContent>
+          <Navbar />
+          {children}
+        </AppContent>
+        <FooterMini />
       </div>
-    </>
+    </div>
   );
 }
